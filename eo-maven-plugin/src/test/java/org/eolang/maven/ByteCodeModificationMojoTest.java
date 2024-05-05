b5478b83f6b8e18e1bd7c3ca0c4dae9a767064ac
Konstantin Lopatko <const42@gmail.com>
2024-05-06 02:45:02 +0300

    Problem with Java version detected - tests in progress

--- eo-maven-plugin/src/test/java/org/eolang/maven/ByteCodeModificationMojoTest.java
+++ eo-maven-plugin/src/test/java/org/eolang/maven/ByteCodeModificationMojoTest.java
@@ -24,6 +24,8 @@
 
 package org.eolang.maven;
 
+import org.eolang.maven.name.ObjectName;
+import org.eolang.maven.name.OnVersioned;
 import org.eolang.maven.util.HmBase;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -48,7 +50,7 @@ public class ByteCodeModificationMojoTest {
     private static final String CLASS_C_PATH_ASM = "org" + File.separator + "eolang" + File.separator + "C";
     private static final String CLASS_C_DESCRIPTOR = "L" + CLASS_C_PATH_ASM + ";";
 
-    public static Path SRC = Paths.get("src/test/resources/org/eolang/maven/bytecode-modification/");
+    public static File SRC = new File("src/test/resources/org/eolang/maven/bytecode-modification/");
 
 
     private static final String RELATIVE_INPUT_DIR = "target" + File.separator + "classes";
@@ -62,14 +64,14 @@ public class ByteCodeModificationMojoTest {
         Path outputDirPath = temp.resolve(RELATIVE_OUTPUT_DIR);
         compile(temp);
 
-        new FakeMaven(temp)
-                .with("inputDir", inputDirPath)
-                .with("outputDir", outputDirPath)
-                .with("hash", HASH)
-                .execute(ByteCodeModificationMojo.class);
+//        new FakeMaven(temp)
+//                .with("inputDir", inputDirPath)
+//                .with("outputDir", outputDirPath)
+//                .with("hash", HASH)
+//                .execute(ByteCodeModificationMojo.class);
 
-        Path outPathClassA = outputDirPath.resolve(HASH).resolve(CLASS_A_PATH_ASM);
-        ClassReader classReader = new ClassReader(Files.readAllBytes(outPathClassA));
+//        Path outPathClassA = outputDirPath.resolve(HASH).resolve(CLASS_A_PATH_ASM);
+//        ClassReader classReader = new ClassReader(Files.readAllBytes(outPathClassA));
     }
 
     private void saveClassFile(
@@ -82,25 +84,34 @@ public class ByteCodeModificationMojoTest {
 
     private void compile(Path outputPath) throws IOException {
 
-        List<Path> output2 = List.of(SRC);
+
+        List<Path> list = Arrays.asList(SRC);
+        final ObjectName text = new OnVersioned("org.eolang.txt.text", "5f82cc1");
+        String format = String.format(
+                "Tojos should contain versioned objects '%s' after probing, but they didn't",
+                Arrays.asList(text, ProbeMojoTest.STDOUT)
+        );
+
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
         StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
         List<Path> output = new ArrayList<>();
         output.add(SRC);
         fileManager.setLocationFromPaths(StandardLocation.CLASS_OUTPUT, output);
-        Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromPaths(Arrays.asList(
-                SRC.resolve("org").resolve("eolang").resolve("A.java"),
-                SRC.resolve("org").resolve("eolang").resolve("B.java"),
-                SRC.resolve("org").resolve("eolang").resolve("C.java"),
-                SRC.resolve("org").resolve("eolang").resolve("Versionized.java")
-        ));
-        compiler.getTask(null,
-                fileManager,
-                null,
-                List.of("-d " + outputPath.toAbsolutePath()),
-                null,
-                compilationUnits
-        ).call();
+        List<File> list1 = Arrays.asList(
+                SRC.resolve("org").resolve("eolang").resolve("A.java").toFile(),
+                SRC.resolve("org").resolve("eolang").resolve("B.java").toFile(),
+                SRC.resolve("org").resolve("eolang").resolve("C.java").toFile(),
+                SRC.resolve("org").resolve("eolang").resolve("Versionized.java").toFile()
+        );
+
+        Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles(list1);
+//        compiler.getTask(null,
+//                fileManager,
+//                null,
+//                List.of("-d " + outputPath.toAbsolutePath()),
+//                null,
+//                compilationUnits
+//        ).call();
     }
 
 
